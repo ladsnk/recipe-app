a2a6f4c8081bd1c7197764025882f341b5db5e95
ladsnk <107194920+ladsnk@users.noreply.github.com>
2022-07-17 18:54:41 +0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import Pages from "./pages/Pages";
+import Category from "./components/Category";
+import { BrowserRouter } from "react-router-dom";
+import Search from "./components/Search";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+import { GiKnifeFork } from "react-icons/gi";
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Nav>
+          <GiKnifeFork />
+          <Logo to={"/"}>kitchen</Logo>
+        </Nav>
+        <Search />
+        <Category />
+        <Pages />
+      </BrowserRouter>
+    </div>
+  );
+}
+
+const Logo = styled(Link)`
+  text-decoration: none;
+  font-size: 3rem;
+  font-weight: 400;
+  font-family: "Lobster Two", cursive;
+`;
+const Nav = styled.div`
+  padding: 4rem 0rem;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  svg {
+    font-size: 4rem;
+  }
+`;
+
+export default App;
